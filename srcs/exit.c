5861849906c1869bf6bd10971358f28dbadb8dc9
Ayoub Krifa <aykrifa@paul-f4Br9s1.clusters.42paris.fr>
2025-02-22 12:06:03 +0100

    commit depuis neovim

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,7 +6,7 @@
 /*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 15:38:03 by aykrifa           #+#    #+#             */
-/*   Updated: 2025/02/21 13:42:24 by aykrifa          ###   ########.fr       */
+/*   Updated: 2025/02/22 10:43:13 by aykrifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ int	exit_fdf(t_data *fdf)
 	free(fdf->mlx);
 	liberator_int_tab(fdf->coordinate);
 	liberator_int_tab(fdf->save);
-	exit(15);
+	exit(0);
 }
 
 void	ft_exit(t_data *fdf, t_list *lst, int err_code)
